05f687d5055cc8660c3a7cd3bb1766cde06d73ba
Saul <saulflores3126@gmail.com>
2020-03-20 12:09:07 -0600

    Pruebas (2)

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -21,24 +21,42 @@ public class Main {
 
 
 
-        for(double i = 1; i == 1000000000; i=i*10){
-            int[] elementos = array.randomArray(i);
 
-            for (int x =0; x<=11; x++){
-                tempMerge = merge.sort(elementos);
-                tempQuick = quick.sort(elementos);
-                tempHeap = heap.sort(elementos);
 
-                arrayMerge[x]= tempMerge;
-                arrayQuick[x]= tempQuick;
-                arrayHeap[x]= tempHeap;
+        for (int i = 10 ; i<=1000000000; i= i*10){
+            int[] arr = array.randomArray(i);
 
+            tempMerge = merge.sort(arr);
+            tempHeap = heap.sort(arr);
+            tempQuick = quick.sort(arr);
+
+            for (int x= 0 ; x<=arr.length-1; x++){
+
+                arrayMerge[x] = tempMerge;
+                arrayHeap[x] = tempHeap;
+                arrayQuick[x] = tempQuick;
             }
-
 
         }
 
-        for (int i =0 ; i<=arrayHeap.length; i++) System.out.println(arrayHeap[i]);
+
+        for (int j=0; j>=arrayHeap.length-1; j++){
+            System.out.println(arrayHeap[j]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //
@@ -52,10 +70,7 @@ public class Main {
 //        int[] elementosOrdenadosHeap = heap.heapSort(elementos);
 //
 //
-//        System.out.println("Merge Sort tarda");
-//        System.out.println("\n\ntarda "+ merge.sort(elementos)+ " milisegundos");
-//
-//        System.out.println("----------------------------------------------------");
+
 //
 //        System.out.println("Quick Sort tarda");
 //        System.out.println("\n\ntarda "+ quick.sort(elementos)+ " milisegundos");
--- src/edu/ujcv/progra1/RandomArray.java
+++ src/edu/ujcv/progra1/RandomArray.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class RandomArray {
     Random random = new Random();
 
-    int[] randomArray(double tamano){
-        int[] arr = new int[(int) tamano];
+    int[] randomArray(int tamano){
+        int[] arr = new int[tamano];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt();
 
